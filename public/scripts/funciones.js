4b66aa186e43cc3cd0371759eeb3fad90063c358
Gabriel Ramirez <gabriel.ramirez.lemus@gmail.com>
2021-02-24 04:27:54 -0300

    Implementacion efecto visual scroll

--- public/scripts/funciones.js
+++ public/scripts/funciones.js
@@ -1,22 +1,28 @@
 function loadDoc(link) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("body").innerHTML = this.responseText;
-      }
-    };
-    
-    xhttp.open("GET", `http://localhost:3000/${link}`, true);
-    xhttp.send();
-  }
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("body").innerHTML = this.responseText;
+    }
+  };
+
+  xhttp.open("GET", `http://localhost:3000/${link}`, true);
+  xhttp.send();
+}
 
 
 
-  window.addEventListener('scroll',()=>{
-    const scrolled = window.scrollY
-    
-    console.log(scrolled);
-  })
+/*  window.addEventListener('scroll',()=>{
+   const scrolled = window.scrollY
+   
+   console.log(scrolled);
+ })
+*/
 
-  
+ScrollReveal({ reset: true }).reveal('.imagen-inicio-correo', { delay: 100 })
+ScrollReveal({ reset: true }).reveal('.imagen-inicio-mecanico', { delay: 100 })
+ScrollReveal({ reset: true }).reveal('.inicio-h1', { delay: 300 })
+
+
+
 
