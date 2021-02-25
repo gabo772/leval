c5638f2946c234c44802a5466de0b77283defbfd
Gabriel Ramirez <gabriel.ramirez.lemus@gmail.com>
2021-02-25 04:17:16 -0300

    Implementacion contenidos restantes, queda footer y otros detalles

--- public/scripts/funciones.js
+++ public/scripts/funciones.js
@@ -21,7 +21,11 @@ function loadDoc(link) {
 
 ScrollReveal({ reset: true }).reveal('.imagen-inicio-correo', { delay: 100 })
 ScrollReveal({ reset: true }).reveal('.imagen-inicio-mecanico', { delay: 100 })
+ScrollReveal({ reset: true }).reveal('.imagen-quienes-somos', { delay: 100 })
 ScrollReveal({ reset: true }).reveal('.inicio-h1', { delay: 300 })
+ScrollReveal({ reset: true }).reveal('.quienes-somos-h1', { delay: 300 })
+ScrollReveal({ reset: true }).reveal('.c-mision', { delay: 200 })
+ScrollReveal({ reset: true }).reveal('.c-vision', { delay: 400 })
 
 
 
