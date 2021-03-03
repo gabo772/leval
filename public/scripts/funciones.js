f13badaf0e291b77eb595f2c2f18543f53236c87
Gabriel Ramirez <gabriel.ramirez.lemus@gmail.com>
2021-03-03 04:24:05 -0300

    Comienzo mobile

--- public/scripts/funciones.js
+++ public/scripts/funciones.js
@@ -1,14 +1,3 @@
-function loadDoc(link) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("body").innerHTML = this.responseText;
-    }
-  };
-
-  xhttp.open("GET", `http://localhost:3000/${link}`, true);
-  xhttp.send();
-}
 
 
 
