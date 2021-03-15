be6409b54972901d51712479d79e9b7cef8445e5
Gabriel <gabriel.ramirez.lemus@gmail.com>
2021-03-15 04:19:59 -0300

    Cambios transiciones

--- public/scripts/funciones.js
+++ public/scripts/funciones.js
@@ -29,14 +29,43 @@ x.addEventListener("",ocultar_mostrar_menu)
 
 window.addEventListener("scroll",(e)=>{
   y = window.scrollY
+  nclass=document.getElementsByClassName("navigator")
+  hclass=document.getElementsByClassName("header")
   console.log(y);
-  if(y<=203){
-    
+  if(y<=278){
+    nclass.className="navigator"
+    hclass.className="header"
   }else{
+    nclass.className="navigator-scroll"
+    hclass.className="header-scroll"
 
+
+    console.log("cambia clase a scroll");
+    console.log(hclass);
   }
 
 })
+
+// if(document.getElementsByClassName("navigator-scroll")[0]){
+//   document.getElementsByClassName("navigator-scroll")[0].addEventListener("transitionend",updateTNavigator,true)
+
+// }
+// if(document.getElementsByClassName("header-scroll")[0]){
+
+//   document.getElementsByClassName("header-scroll")[0].addEventListener("transitionend",updateTHeader,true)
+// }
+
+// const updateTNavigator=()=>{
+//   var el = document.querySelector("div.navigator-scroll");
+//   el.className = "navigator-scroll-end";
+    
+// }
+
+// const updateTHeader=()=>{
+//   var el = document.querySelector("div.header-scroll");
+//   el.className = "header-scroll-end";
+    
+// }
 
 ScrollReveal({ reset: true }).reveal('.imagen-inicio-correo', { delay: 100 })
 ScrollReveal({ reset: true }).reveal('.imagen-inicio-mecanico', { delay: 100 })
